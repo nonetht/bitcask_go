4be8020c17ebbe6057e0e2855cd848ec9659fc47
yang <94279769+nonetht@users.noreply.github.com>
2025-12-26 12:31:21 +0800

    define IOManager as interface and use os.File implement the interface

--- fio/file_io.go
+++ fio/file_io.go
@@ -1,1 +1,23 @@
 package fio
+
+import "os"
+
+type FileIO struct {
+	f *os.File // 为什么选择 *os.File 而不是 os.File
+}
+
+func (fio FileIO) Read(key []byte, offset int64) (int, error) {
+	return fio.f.ReadAt(key, offset)
+}
+
+func (fio FileIO) Write(key []byte) (int, error) {
+	return fio.f.WriteAt(key, 0)
+}
+
+func (fio FileIO) Sync() error {
+	return fio.f.Sync()
+}
+
+func (fio FileIO) Close() error {
+	return fio.f.Close()
+}

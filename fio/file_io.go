a514491f56b824d5f5a1f9fadca79e490ed51503
yang <94279769+nonetht@users.noreply.github.com>
2025-12-26 12:45:56 +0800

    correct the wrong things

--- fio/file_io.go
+++ fio/file_io.go
@@ -6,18 +6,31 @@ type FileIO struct {
 	f *os.File // 为什么选择 *os.File 而不是 os.File
 }
 
-func (fio FileIO) Read(key []byte, offset int64) (int, error) {
+func NewFileIOManager(fileName string) (*FileIO, error) {
+	f, err := os.OpenFile(
+		fileName,
+		os.O_CREATE|os.O_RDWR|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileIO{f: f}, nil
+}
+
+func (fio *FileIO) Read(key []byte, offset int64) (int, error) {
 	return fio.f.ReadAt(key, offset)
 }
 
-func (fio FileIO) Write(key []byte) (int, error) {
+func (fio *FileIO) Write(key []byte) (int, error) {
 	return fio.f.Write(key)
 }
 
-func (fio FileIO) Sync() error {
+func (fio *FileIO) Sync() error {
 	return fio.f.Sync()
 }
 
-func (fio FileIO) Close() error {
+func (fio *FileIO) Close() error {
 	return fio.f.Close()
 }

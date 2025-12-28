7a5d7187381d3c2a0a40636ce2cc165d02c7f2e0
yang <94279769+nonetht@users.noreply.github.com>
2025-12-28 17:45:58 +0800

    create DataFile struct and appendLogRecord method

--- data/data_file.go
+++ data/data_file.go
@@ -1,1 +1,14 @@
 package data
+
+import "bitcask-gown/fio"
+
+// DataFile 负责文件部分内容，
+type DataFile struct {
+	FileID    uint32        // 文件 ID 号
+	WriteOff  int64         // 告知对于当前文件，已经写入到了哪里
+	IoManager fio.IOManager // 命名基于它是用来读写字节的
+}
+
+func (d DataFile) appendLogRecord(record *LogRecord) {
+
+}

4af09e5c6f0f84ffc2d73b69139f69d7bee8356e
yang <94279769+nonetht@users.noreply.github.com>
2025-12-28 21:33:45 +0800

    implemented NewDataFile and OpenDataFile function

--- data/data_file.go
+++ data/data_file.go
@@ -1,14 +1,36 @@
 package data
 
-import "bitcask-gown/fio"
+import (
+	"bitcask-gown/fio"
+	"fmt"
+	"path/filepath"
+)
+
+const DataFileNameSuffix = ".data"
 
 // DataFile 负责文件部分内容，
 type DataFile struct {
 	FileID    uint32        // 文件 ID 号
 	WriteOff  int64         // 告知对于当前文件，已经写入到了哪里
-	IoManager fio.IOManager // 命名基于它是用来读写字节的
+	IOManager fio.IOManager // 命名基于它是用来读写字节的
 }
 
-func (d DataFile) appendLogRecord(record *LogRecord) {
+// OpenDataFile 打开或创建新的数据文件
+func OpenDataFile(dirPath string, fileId uint32) (*DataFile, error) {
+	// 1. 拼接文件名，例如：/tmp/bitcask/000000001.data
+	fileName := filepath.Join(dirPath, fmt.Sprintf("%09d.data", fileId)+DataFileNameSuffix)
+	return NewDataFile(fileName, fileId)
+}
 
+func NewDataFile(fileName string, fileId uint32) (*DataFile, error) {
+	ioManager, err := fio.NewFileIOManager(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DataFile{
+		FileID:    fileId,
+		WriteOff:  0,
+		IOManager: ioManager,
+	}, nil
 }
